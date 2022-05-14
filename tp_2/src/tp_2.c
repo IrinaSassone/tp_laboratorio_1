941fbc8636d02032b4e8913b33092ae0a2c66de1
IrinaSassone <irusassone@gmail.com>
2022-05-14 16:22:17 -0300

    2 commits

---
+++ tp_2/src/tp_2.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : tp_2.c
+ Author      : Sassone Irina
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("tp_2"); /* prints tp_2 */
+	return EXIT_SUCCESS;
+}
