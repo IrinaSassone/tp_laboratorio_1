006b39fbfe6400a3a69ccf9d3051a759b3c9717b
IrinaSassone <irusassone@gmail.com>
2022-05-14 16:18:52 -0300

    Algo

---
+++ tp_1/src/tp_1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : tp_1.c
+ Author      : Sassone Irina
+ Version     :
+ Copyright   : Your copyright notice
+ Description : tp_1 in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
